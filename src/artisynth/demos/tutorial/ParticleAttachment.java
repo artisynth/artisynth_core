e9290cdbe751978fd085fdf1c00e74a43f88d9b4
John Lloyd <lloyd@cs.ubc.ca>
2014-09-18 16:31:05 +0000

    added ParticleAttachment demo
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@122 7661e47b-f193-4b25-995f-cdbae50cbfef

---
+++ src/artisynth/demos/tutorial/ParticleAttachment.java
@@ -0,0 +1,76 @@
+package artisynth.demos.tutorial;
+
+import java.awt.Color;
+
+import maspack.matrix.*;
+import maspack.render.*;
+
+import artisynth.core.mechmodels.*;
+import artisynth.core.materials.*;
+import artisynth.core.workspace.RootModel;
+
+/**
+ * Simple demo of a particle and rigid body connected by a spring.
+ */
+public class ParticleAttachment extends RootModel {
+
+   public Particle addParticle (MechModel mech, double x, double y, double z) {
+      // create a particle at x, y, z and add it to mech
+      Particle p = new Particle (/*name=*/null, /*mass=*/.1, x, y, z);
+      mech.addParticle (p);
+      return p;
+   }
+
+   public AxialSpring addSpring (MechModel mech, Particle p1, Particle p2){
+      // create a spring connecting p1 and p2 and add it to mech
+      AxialSpring spr = new AxialSpring (/*name=*/null, /*restLength=*/0);
+      spr.setMaterial (new LinearAxialMaterial (/*k=*/20, /*d=*/10));
+      spr.setPoints (p1, p2);
+      mech.addAxialSpring (spr);
+      return spr;
+   }
+
+   public void build (String[] args) {
+
+      // create MechModel and add to RootModel
+      MechModel mech = new MechModel ("mech");
+      addModel (mech);
+
+      // create the components
+      Particle p1 = addParticle (mech, 0, 0, 0.55);
+      Particle p2 = addParticle (mech, 0.1, 0, 0.35);
+      Particle p3 = addParticle (mech, 0.1, 0, 0.35);
+      Particle p4 = addParticle (mech, 0, 0, 0.15);
+      addSpring (mech, p1, p2);
+      addSpring (mech, p3, p4);
+      // create box and set it's pose (position/orientation):
+      RigidBody box =
+         RigidBody.createBox ("box", /*wx,wy,wz=*/0.5, 0.3, 0.3, /*density=*/20);
+      box.setPose (new RigidTransform3d (/*x,y,z=*/0.2, 0, 0));
+      mech.addRigidBody (box);
+
+      p1.setDynamic (false);               // first particle set to be fixed
+
+      // set up the attachments
+      mech.attachPoint (p2, p3);
+      mech.attachPoint (p4, box, new Point3d (0, 0, 0.15));
+
+      // increase model bounding box for the viewer
+      mech.setBounds (/*min=*/-0.5, 0, -0.5, /*max=*/0.5, 0, 0);  
+      // set render properties for the components
+      setPointRenderProps (mech);
+      setLineRenderProps (mech);
+   }
+
+   protected void setPointRenderProps (Renderable r) {
+      RenderProps.setPointColor (r, Color.RED);
+      RenderProps.setPointStyle (r, RenderProps.PointStyle.SPHERE);
+      RenderProps.setPointRadius (r, 0.06);
+   }
+
+   protected void setLineRenderProps (Renderable r) {
+      RenderProps.setLineColor (r, Color.BLUE);
+      RenderProps.setLineStyle (r, RenderProps.LineStyle.CYLINDER);
+      RenderProps.setLineRadius (r, 0.02);
+   }
+}

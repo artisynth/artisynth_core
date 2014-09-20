b56f4f0e872b8fb230539887c153218e49488e40
John Lloyd <lloyd@cs.ubc.ca>
2014-09-20 16:56:40 +0000

    updates to modelguide
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@126 7661e47b-f193-4b25-995f-cdbae50cbfef

--- src/artisynth/demos/tutorial/SimpleMuscleWithPanel.java
+++ src/artisynth/demos/tutorial/SimpleMuscleWithPanel.java
@@ -1,22 +1,24 @@
 package artisynth.demos.tutorial;
 
+import javax.swing.JSeparator;
 import java.io.IOException;
 import artisynth.core.gui.*;
 
-public class SimpleMuscleWithPanel extends SimpleMuscle
-{
-
+public class SimpleMuscleWithPanel extends SimpleMuscle {
    ControlPanel panel;
 
    public void build (String[] args) throws IOException {
 
       super.build (args);
 
-      // add control panel to control muscle
-
-      panel = new ControlPanel();
+      // add control panel for gravity, rigod body mass and color, and excitation
+      panel = new ControlPanel("controls");
+      panel.addWidget (mech, "gravity");
+      panel.addWidget (mech, "rigidBodies/box:mass");
+      panel.addWidget (mech, "rigidBodies/box:renderProps.faceColor");
+      panel.addWidget (new JSeparator());
       panel.addWidget (muscle, "excitation");
+
       addControlPanel (panel);
    }
-
 }

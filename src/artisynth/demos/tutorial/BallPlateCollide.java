e482798b4f6f7f4adccaa6e80fb3d96c69aec0fa
John Lloyd <lloyd@cs.ubc.ca>
2014-10-25 20:28:56 +0000

    added BallPlateCollide to tutorials
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@170 7661e47b-f193-4b25-995f-cdbae50cbfef

---
+++ src/artisynth/demos/tutorial/BallPlateCollide.java
@@ -0,0 +1,54 @@
+package artisynth.demos.tutorial;
+
+import java.awt.Color;
+
+import maspack.matrix.*;
+import maspack.render.*;
+import artisynth.core.workspace.*;
+import artisynth.core.mechmodels.*;
+
+public class BallPlateCollide extends RootModel {
+
+   public void build (String[] args) {
+
+      System.out.println (args.length+" args:");
+      for (String a : args) {
+         System.out.println (" "+a);
+      }
+
+      // create MechModel and add to RootModel
+      MechModel mech = new MechModel ("mech");
+      addModel (mech);
+
+      // create the components
+
+      // create and add the ball and plate and b
+      RigidBody ball = RigidBody.createIcosahedralSphere ("ball", 0.8, 0.1, 1);
+      ball.setPose (new RigidTransform3d (0, 0, 2, 0.4, 0.1, 0.1));
+      mech.addRigidBody (ball);
+      RigidBody plate = RigidBody.createBox ("plate", 5, 5, 0.4, 1);
+      plate.setDynamic (false);
+      mech.addRigidBody (plate);
+
+      // turn on collisions
+      mech.setDefaultCollisionBehavior (true, 0.20);
+
+      // make ball transparent so that contacts can be seen more clearly
+      RenderProps.setFaceStyle (ball, RenderProps.Faces.NONE);
+      RenderProps.setDrawEdges (ball, true);
+      RenderProps.setEdgeColor (ball, Color.WHITE);
+
+      // enable rendering of contacts normals and contours
+      CollisionManager cm = mech.getCollisionManager();
+      RenderProps.setVisible (cm, true);
+      RenderProps.setLineWidth (cm, 3);      
+      RenderProps.setLineColor (cm, Color.RED);
+      RenderProps.setEdgeWidth (cm, 3);
+      RenderProps.setEdgeColor (cm, Color.BLUE);
+      cm.setContactNormalLen (0.5);
+      cm.setDrawIntersectionContours (true);
+
+   }
+
+
+}

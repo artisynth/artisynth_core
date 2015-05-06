a8b7feeff34b0d1c4fb4a56833fc4d743d7b5ea6
Ian Stavness <stavness@gmail.com>
2015-05-06 17:57:12 +0000

    added demo for penetration renderer
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@285 7661e47b-f193-4b25-995f-cdbae50cbfef

---
+++ src/artisynth/demos/tutorial/PenetrationRenderer.java
@@ -0,0 +1,66 @@
+package artisynth.demos.tutorial;
+
+import maspack.properties.PropertyList;
+import maspack.render.GLRenderer;
+import maspack.render.RenderList;
+import maspack.render.RenderProps;
+import artisynth.core.mechmodels.CollisionHandler;
+import artisynth.core.modelbase.MonitorBase;
+
+public class PenetrationRenderer extends MonitorBase {
+
+   CollisionHandler myCollisionHandler;
+   
+   public PenetrationRenderer (CollisionHandler handler) {
+      super ();
+      myCollisionHandler = handler;
+      setRenderProps (createRenderProps ());
+   }
+
+   protected RenderProps myRenderProps = null;
+
+   public static PropertyList myProps = new PropertyList (
+      PenetrationRenderer.class, MonitorBase.class);
+
+   static private RenderProps defaultRenderProps = new RenderProps ();
+
+   static {
+      myProps.add (
+         "renderProps * *", "render properties for this constraint",
+         defaultRenderProps);
+   }
+
+   public PropertyList getAllPropertyInfo () {
+      return myProps;
+   }
+
+   @Override
+   public void apply (double t0, double t1) {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void prerender (RenderList list) {
+      super.prerender (list);
+      System.out.println ("PR - prerender");
+   }
+
+   @Override
+   public void render (GLRenderer gl, int flags) {
+      super.render (gl, flags);
+      System.out.println ("PR - render");
+
+   }
+
+   @Override
+   public RenderProps createRenderProps () {
+      return super.createRenderProps ();
+   }
+
+   @Override
+   public boolean isSelectable () {
+      return true;
+   }
+
+}
---
+++ src/artisynth/demos/tutorial/PenetrationRendererDemo.java
@@ -0,0 +1,70 @@
+package artisynth.demos.tutorial;
+
+import java.awt.Color;
+
+import maspack.matrix.Point3d;
+import maspack.matrix.RigidTransform3d;
+import maspack.render.GLRenderer;
+import maspack.render.RenderProps;
+import artisynth.core.driver.Main;
+import artisynth.core.mechmodels.CollisionHandler;
+import artisynth.core.mechmodels.CollisionManager;
+import artisynth.core.mechmodels.MechModel;
+import artisynth.core.mechmodels.RigidBody;
+import artisynth.core.workspace.DriverInterface;
+import artisynth.core.workspace.RootModel;
+
+public class PenetrationRendererDemo extends RootModel {
+
+   public void build (String[] args) {
+
+      // create MechModel and add to RootModel
+      MechModel mech = new MechModel ("mech");
+      addModel (mech);
+
+      // create and add the ball and plate
+      RigidBody ball = RigidBody.createIcosahedralSphere ("ball", 0.8, 0.1, 1);
+//      ball.setPose (new RigidTransform3d (0, 0, 2, 0.4, 0.1, 0.1));
+      ball.setPose (new RigidTransform3d (0, 0, 0, 0.4, 0.1, 0.1));
+      ball.setDynamic (false);
+      mech.addRigidBody (ball);
+      RigidBody plate = RigidBody.createBox ("plate", 5, 5, 5, 1);
+      plate.setPose (new RigidTransform3d (0, 0, 2.94, 1, 0, 0, 0));
+      plate.setDynamic (false);
+      mech.addRigidBody (plate);
+
+      // turn on collisions
+      mech.setDefaultCollisionBehavior (true, 0.20);
+
+      // make ball transparent so that contacts can be seen more clearly
+      RenderProps.setFaceStyle (ball, RenderProps.Faces.NONE);
+      RenderProps.setDrawEdges (ball, true);
+      RenderProps.setEdgeColor (ball, Color.WHITE);
+      
+      RenderProps.setVisible (plate, false);
+      RenderProps.setAlpha(plate, 0.5);
+
+      // enable rendering of contacts normals and contours
+      CollisionManager cm = mech.getCollisionManager();
+      RenderProps.setVisible (cm, true);
+      RenderProps.setLineWidth (cm, 3);      
+      RenderProps.setLineColor (cm, Color.RED);
+      RenderProps.setEdgeWidth (cm, 3);
+      RenderProps.setEdgeColor (cm, Color.BLUE);
+      cm.setContactNormalLen (0.5);
+      cm.setDrawIntersectionContours (true);
+      cm.setDrawIntersectionFaces (true);
+      
+      addMonitor (new PenetrationRenderer (cm.collisionHandlers ().get (0)));
+   }
+   
+   @Override
+   public void attach (DriverInterface driver) {
+      super.attach (driver);     
+      
+      // set the default camera view to show the intersection
+      Main.getMain ().getViewer ().setEye (new Point3d(-1.64363, -3.53706, 3.008));
+      Main.getMain ().getViewer ().setCenter (new Point3d());
+   }
+   
+}

be96b84e95529b7a1054feb8fb9de80eff47a866
Antonio Sanchez <antonios@ece.ubc.ca>
2016-07-28 01:18:00 +0000

    fixing of crypto stuff which failed on windows
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@699 7661e47b-f193-4b25-995f-cdbae50cbfef

--- src/maspack/crypt/CryptorBase.java
+++ src/maspack/crypt/CryptorBase.java
@@ -10,29 +10,28 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class CryptorBase implements Cryptor {
-   
+
    private static String DEFAULT_CIPHER = "AES";
    private static String DEFAULT_PASSPHRASE = "wG@.9Gz}65qLS}e<%/Vpt-m55$FY[Drq";
-   private static String DEFAULT_IV = "0xca3608f50b34c4846560ccfb77f11faa";
-   
+
    private SecretKeySpec keySpec;
    private IvParameterSpec ivSpec;
    private Cipher cipher;
-   
+
    public CryptorBase() {
       this(null, null, null);
    }
-   
+
    public CryptorBase(Cipher cipher, SecretKeySpec keySpec, IvParameterSpec ivSpec) {
       this.cipher = cipher;
       this.keySpec = keySpec;
       this.ivSpec = ivSpec;
    }
-   
+
    protected void setCipher(Cipher cipher) {
       this.cipher = cipher;
    }
-   
+
    protected void setCipher(String cipher) throws NoSuchAlgorithmException, NoSuchPaddingException {
       this.cipher = Cipher.getInstance(cipher);
    }
@@ -40,7 +39,7 @@ public class CryptorBase implements Cryptor {
    protected void setKey(SecretKeySpec keySpec) {
       this.keySpec = keySpec;
    }
-   
+
    public void setKey(byte[] key) {
       if (key == null) {
          key = generateKeyFromPassphrase(DEFAULT_PASSPHRASE);
@@ -51,13 +50,7 @@ public class CryptorBase implements Cryptor {
             // adjust key length to be maximum, repeating key if necessary
             if (key.length > max) {
                key = Arrays.copyOf(key, max);
-            } else if (key.length < max) {
-               byte[] newkey = new byte[max];
-               for (int i=0; i<max; ++i) {
-                  newkey[i] = key[i%key.length];
-               }
-               key = newkey;
-            }
+            } 
             String[] algorithm = cipher.getAlgorithm().split("/");
             keySpec = new SecretKeySpec(key, algorithm[0]);
          } catch (NoSuchAlgorithmException e) {
@@ -65,7 +58,7 @@ public class CryptorBase implements Cryptor {
          }   
       }
    }
-   
+
    public void setKey(String base64key) throws Exception {
       byte[] key = Base64.decode(base64key);
       setKey(key);
@@ -92,7 +85,7 @@ public class CryptorBase implements Cryptor {
 
       MessageDigest digest;
       byte[] key = null;
-      
+
       try {
          digest = MessageDigest.getInstance("SHA-256");
          digest.reset();
@@ -106,7 +99,7 @@ public class CryptorBase implements Cryptor {
       return key;
 
    }
-   
+
    /**
     * Sets initialization vector for algorithm
     * @param ivSpec
@@ -114,15 +107,19 @@ public class CryptorBase implements Cryptor {
    public void setIV(IvParameterSpec ivSpec) {
       this.ivSpec = ivSpec;
    }
-   
+
    /**
     * Sets initialization vector for algorithm
     * @param bytes
     */
    public void setIV(byte[] bytes) {
-      ivSpec = new IvParameterSpec(bytes);
+      if (bytes == null) {
+         ivSpec = null;
+      } else {
+         ivSpec = new IvParameterSpec(bytes);
+      }
    }
-   
+
    /**
     * Sets initialization vector for algorithm with a given
     * length.  The bytes are either trimmed or looped.
@@ -139,9 +136,9 @@ public class CryptorBase implements Cryptor {
          }
          ivSpec = new IvParameterSpec(newbytes);
       }
-      
+
    }
-   
+
    /**
     * Sets initialization vector
     * @param iv64 base64-encoded bytes
@@ -149,7 +146,7 @@ public class CryptorBase implements Cryptor {
    public void setIV(String iv64) {
       setIV(Base64.decode(iv64));
    }
-   
+
    /**
     * Sets initialization vector
     * @param ivHex base16-encoded bytes
@@ -157,7 +154,7 @@ public class CryptorBase implements Cryptor {
    public void setIVHex(String ivHex) {
       setIV(Base16.decode(ivHex));
    }
-   
+
    // encrypt
    @Override
    public String encrypt(String str) {
@@ -176,28 +173,29 @@ public class CryptorBase implements Cryptor {
       if (cipher == null) {
          try {
             setCipher(DEFAULT_CIPHER);
+            if (keySpec == null) {
+               setKeyFromPassphrase(DEFAULT_PASSPHRASE);
+            }
          } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
             throw new RuntimeException("Failed to initialize cryptor", e);
          }
       }
-      if (keySpec == null) {
-         setKeyFromPassphrase(DEFAULT_PASSPHRASE);
-      }
-      if (ivSpec == null) {
-         setIVHex(DEFAULT_IV);
-      }
-      
-      if (ivSpec.getIV().length != keySpec.getEncoded().length) {
-         // adjust iv
-         setIV(ivSpec.getIV(), keySpec.getEncoded().length);
-      }
-      
+
       byte[] out = null;
       try {
-         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-         out = new byte[cipher.getOutputSize(data.length)];
-         int outLength = cipher.update(data, 0, data.length, out, 0);
-         outLength += cipher.doFinal(out, outLength);
+         if (keySpec != null) {
+            if (ivSpec != null) {
+               cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+            } else {
+               cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            }
+            out = new byte[cipher.getOutputSize(data.length)];
+            int outLength = cipher.update(data, 0, data.length, out, 0);
+            outLength += cipher.doFinal(out, outLength);
+         } else {
+            out = data;
+         }
+
       } catch (Exception e) {
          throw new RuntimeException("encryption failed", e);
       }
@@ -210,24 +208,30 @@ public class CryptorBase implements Cryptor {
       if (cipher == null) {
          try {
             setCipher(DEFAULT_CIPHER);
+            if (keySpec == null) {
+               setKeyFromPassphrase(DEFAULT_PASSPHRASE);
+            }
          } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
             throw new RuntimeException("Failed to initialize cryptor", e);
          }
       }
-      if (keySpec == null) {
-         setKeyFromPassphrase(DEFAULT_PASSPHRASE);
-      }
-      if (ivSpec == null) {
-         setIVHex(DEFAULT_IV);
-      }
-      
+
       byte[] out = null;
       try {
-         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-         out = new byte[cipher.getOutputSize(data.length)];
-         int outLen = cipher.update(data, 0, data.length, out, 0);
-         outLen += cipher.doFinal(out, outLen);
-         out = Arrays.copyOf(out, outLen);
+         if (keySpec != null) {
+            if (ivSpec != null) {
+               cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);   
+            } else {
+               cipher.init(Cipher.DECRYPT_MODE, keySpec);
+            }
+            out = new byte[cipher.getOutputSize(data.length)];
+            int outLen = cipher.update(data, 0, data.length, out, 0);
+            outLen += cipher.doFinal(out, outLen);
+            out = Arrays.copyOf(out, outLen);
+         } else {
+            out = data;
+         }
+
       } catch (Exception e) {
          throw new RuntimeException("decryption failed", e);
       }

5a0d1c717e3452451e4ccb09d0028a138d234cda
John Lloyd <lloyd@cs.ubc.ca>
2017-05-09 21:13:32 +0000

    New code to draw labels on grids
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@882 7661e47b-f193-4b25-995f-cdbae50cbfef

---
+++ src/maspack/render/Dragger3dConstrainer.java
@@ -0,0 +1,43 @@
+package maspack.render;
+
+import maspack.matrix.RigidTransform3d;
+import maspack.matrix.RotationMatrix3d;
+import maspack.matrix.Vector3d;
+
+/**
+ * Experimental object to allow dragger motions to be constrained in arbitrary
+ * ways as specified by the application.
+ */
+public interface Dragger3dConstrainer {
+
+   /**
+    * Takes a new value for the dragger rotation matrix and updates it to one
+    * that satifies any constraints. The previous dragger transform is provided
+    * for reference. If the constraint is such that the rotation matrix is
+    * simply reset to its original value, the method should return false.
+    *
+    * @param RDWnew new value for the dragger rotation matrix, which should be
+    * modified if necessary
+    * @param TDW previous dragger-to-world transform.
+    * @return <code>false</code> if there is no motion and <code>RDWnew</code>
+    * is reset to its original value <code>TDW.R</code>.
+    */
+   public boolean updateRotation (
+      RotationMatrix3d RDW, RigidTransform3d TDW);
+
+   /**
+    * Takes a new value for the dragger translation and updates it to one that
+    * satifies any constraints. The previous dragger transform is provided for
+    * reference. If the constraint is such that the translation is simply reset
+    * to its original value, the method should return false.
+    *
+    * @param pnew new value for the dragger translation, which should be
+    * modified if necessary
+    * @param TDW previous dragger-to-world transform.
+    * @return <code>false</code> if there is no motion and <code>pnew</code> is
+    * reset to its original value <code>TDW.p</code>.
+    */
+   public boolean updatePosition (
+      Vector3d pnew, RigidTransform3d TDW);
+
+}

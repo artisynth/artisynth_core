4b783b8325486aa1e00382d744bb5e475db5c3b2
John Lloyd <lloyd@cs.ubc.ca>
2017-05-24 18:09:25 +0000

    added getPose() to Wrappable
    git-svn-id: https://svn.artisynth.org/svn/artisynth_core/trunk@897 7661e47b-f193-4b25-995f-cdbae50cbfef

--- src/artisynth/core/mechmodels/Wrappable.java
+++ src/artisynth/core/mechmodels/Wrappable.java
@@ -1,10 +1,13 @@
 package artisynth.core.mechmodels;
 
 import maspack.matrix.Matrix3d;
+import maspack.matrix.RigidTransform3d;
 import maspack.matrix.Point3d;
 import maspack.matrix.Vector3d;
 
 public interface Wrappable extends PointAttachable {
+
+   public final double OUTSIDE = Double.MAX_VALUE;
 
    /**
     * Computes the point <code>pt</code> on the surface of this Wrappable such
@@ -33,20 +36,24 @@ public interface Wrappable extends PointAttachable {
    /**
     * Computes the penetration distance of a point <code>p0</code> into this
     * Wrappable, along with the normal <code>nrm</code> that points from
-    * <code>p0</code> to its nearest point on the surface. If possible,
-    * the method should also compute the derivative of the normal with
-    * respect to changes in <code>p0</code>, and return this in 
-    * <code>dnrm</code>. If this is not possible, <code>dnrm</code>
-    * should be set to 0. The returned distance should be negative. 
-    * If <code>p0</code> is not penetrating, the method should return 0 as 
-    * quickly as it can.
+    * <code>p0</code> to its nearest point on the surface. The distance should
+    * be negative if the point is inside the wrappable, and positive otherwise.
+    * If the point is outside and further than some minimum distance,
+    * then the method should return the constant {@link #OUTSIDE}.
+    * 
+    * <p>If possible, the method should also compute the derivative of the
+    * normal with respect to changes in <code>p0</code>, and return this in
+    * <code>dnrm</code>. If this is not possible, <code>dnrm</code> should be
+    * set to 0.
     * 
     * @param nrm returns the normal (should be normalized)
     * @param dnrm returns the derivative of the normal with respect to 
     * changes in p0, or zero if this cannot be determined.
     * @param p0 point to determine penetration for
-    * @return returns the penetration distance, or 0 if p0 is
-    * not penetrating.
+    * @return returns the penetration distance, or {@link #OUTSIDE} if the
+    * point is further than some minimum distance from the wrappable.
     */
    public double penetrationDistance (Vector3d nrm, Matrix3d dnrm, Point3d p0);
+   
+   public RigidTransform3d getPose();
 }

097f9a5704d8fc080bc8ef77780806c0e96c6432
John Lloyd <lloyd@cs.ubc.ca>
2019-07-21 15:23:38 -0700

    Deleted obsolete classes

--- src/artisynth/core/modelbase/ElementField.java
+++
@@ -1,9 +0,0 @@
-package artisynth.core.modelbase;
-
-import maspack.matrix.VectorObject;
-
-public interface ElementField<T extends VectorObject<T>> extends VectorField<T> {
-
-   public T getValue (int elemNum);
-
-}

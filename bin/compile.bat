03ab0c2d9daaded38a206f97542afb8fdfdec716
John Lloyd <lloyd@cs.ubc.ca>
2020-01-23 17:00:59 -0800

    removed CLASSPATH setting from compile.bat

--- bin/compile.bat
+++ bin/compile.bat
@@ -3,7 +3,8 @@
 @if DEFINED JAVA_HOME (
 	set JAVAC=%JAVA_HOME%\bin\javac
 )
-@set CLASSPATH=%AH%\classes;%AH%\lib\*
+@set CP=%AH%\classes;%AH%\lib\*
 @dir /b /a /s *java > _sources_.txt
-%JAVAC% -J-Xms500m -J-Xmx500m -d %AH%\classes -cp %CLASSPATH% -sourcepath %AH%\src -source 1.8 -target 1.8 -encoding UTF-8 @_sources_.txt
+%JAVAC% -J-Xms500m -J-Xmx500m -d %AH%\classes -cp %CP% -sourcepath %AH%\src -source 1.8 -target 1.8 -encoding UTF-8 @_sources_.txt
 @del /f _sources_.txt
+@set "CP="

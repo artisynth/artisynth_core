3f6a561de05253c2e249e1b260d1b0a42aeef9ed
John Lloyd <lloyd@cs.ubc.ca>
2021-09-09 11:45:23 -0700

    Updates for artisynth.core.util:
    * ArtisyntPath: added set/getWorkingFolder() to replace
      set/getWorkingDir(), methods for setting, accessing and creating the
      user configuration folder, and the method createDefaultSearchPath().
    * ArtisynthIO: added convenience methods save() and load() for saving
      and loading scannable objects
    * ArtisynthDataManager: cache folder is now obtained from the
      user configuration folder

--- src/artisynth/core/util/ArtisynthIO.java
+++ src/artisynth/core/util/ArtisynthIO.java
@@ -86,4 +86,78 @@ public class ArtisynthIO {
       return rtok;
    }
 
+
+   // convenience methods for saving and loading Scannable objects,
+   // while catching any IOExceptions
+
+   public static boolean load (Scannable obj, File file, String name) {
+      boolean success = false;
+      if (file != null) {
+         ReaderTokenizer rtok = null;
+         try {
+            rtok = ArtisynthIO.newReaderTokenizer (file);
+            obj.scan (rtok, null);
+            success = true;
+         }
+         catch (IOException e) {
+            System.out.println (
+               "WARNING: can't load "+name+" file"+file+": "+e);
+         }
+         finally {
+            if (rtok != null) {
+               rtok.close();
+            }
+         }
+      }
+      return success;
+   }
+
+   public static boolean save (Scannable obj, File file, String name) {
+      boolean success = false;
+      if (file != null) {
+         PrintWriter pw = null;
+         try {
+            pw = ArtisynthIO.newIndentingPrintWriter (file);
+            obj.write (pw, new NumberFormat("%g"), null);
+            success = true;
+         }
+         catch (IOException e) {
+            System.out.println (
+               "WARNING: can't save "+name+" file "+file+": "+e);
+         }
+         finally {
+            if (pw != null) {
+               pw.close();
+            }
+         }
+      }
+      return success;
+   }
+
+   public static boolean loadOrCreate (Scannable obj, File file, String name) {
+      if (file != null) {
+         if (file.exists()) {
+            if (!file.canRead()) {
+               System.out.println (
+                  "WARNING: "+name+" file "+file+" is not readable");
+            }
+            else if (load (obj, file, name)) {
+               return true;
+            }
+            else {
+               if (save (obj, file, name)) {
+                  System.out.println ("Reinitialized "+name+" file "+file);
+               }
+            }
+         }
+         else {
+            if (save (obj, file, name)) {
+               System.out.println (
+                  "Initialized "+name+" file "+file);
+               return true;
+            }
+         }
+      }
+      return false;
+   }
 }

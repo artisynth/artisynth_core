ed6d79e79b1b90808a681ea3718b9f7d01cd7625
John Lloyd <lloyd@cs.ubc.ca>
2021-09-16 20:51:48 -0700

    Added Blankevoort1991AxialLigament and Millard2012AxialMuscle

--- src/artisynth/core/materials/AxialMaterialTest.java
+++ src/artisynth/core/materials/AxialMaterialTest.java
@@ -12,61 +12,9 @@ public class AxialMaterialTest extends UnitTest {
    AxialMaterialTest() {
    }
 
-   public void testMaterial (AxialMaterial mat, double l0, double tol) {
-      testMaterial (mat, mat, 2*l0, 0.2*l0, l0, tol);
-   }
-
-   public void testOverRange (
-      AxialMaterial mat, AxialMaterial matdot,
-      double lmin, double lmax, double l0, double tol) {
-
-      int ndivs = 10;
-      double ldot = 0.1*l0;
-      for (int i=0; i<=ndivs;  i++) {
-         double l = lmin + i*(lmax-lmin)/ndivs;
-         testDfdl (mat, l, ldot, l0, tol);
-         testDfdldot (matdot, l, ldot, l0, tol);
-     }
-   }
-
-   /**
-    * Tests a material by comparing the computed derivatives with the numeric
-    * derivative of the same. Different materials are specified for testing
-    * dFdl and dFdldot in case different parameter settings are needed to
-    * numerically compute the different derivates.
-    *
-    * @param mat material to test for dFdl
-    * @param matdot material to test for dFdldot
-    * @param tol tolerance by which the computed and numeric derivatives should
-    * match. Typically, this should be around 1e-8.
-    */
-   public void testMaterial (
-      AxialMaterial mat, AxialMaterial matdot,
-      double l, double ldot, double l0, double tol) {
-
-      testDfdl (mat, l, ldot, l0, tol);
-      testDfdldot (matdot, l, ldot, l0, tol);
-   }
-
-   /**
-    * Tests a material by comparing the computed derivatives with the numeric
-    * derivative of the same.
-    *
-    * @param mat material to test
-    * @param tol tolerance by which the computed and numeric derivatives should
-    * match. Typically, this should be around 1e-8.
-    */
-   public void testMaterial (
-      AxialMaterial mat, double l, double ldot, double l0, double tol) {
-
-      testDfdl (mat, l, ldot, l0, tol);
-      testDfdldot (mat, l, ldot, l0, tol);
-   }
-
-   public void testDfdldot (
-      AxialMaterial mat, double l, double ldot, double l0, double tol) {
-
-      double ex = 0.5;
+   public void checkDfdldot (
+      AxialMaterial mat, double l, double ldot,
+      double l0, double ex, double maxval, double tol) {
 
       double h = 1e-8;
 
@@ -74,10 +22,9 @@ public class AxialMaterialTest extends UnitTest {
       double dFdldot = mat.computeDFdldot (l, ldot, l0, ex);
       double dFdldotChk = (mat.computeF (l, ldot+h*ldot, l0, ex) - F)/(h*ldot);
 
-      double mag = Math.abs(dFdldot);
       double err = Math.abs(dFdldot-dFdldotChk);
-      if (mag > 0) {
-         err /= mag;
+      if (maxval > 0) {
+         err /= maxval;
       }
       if (err > tol || err != err) {
          System.out.println (
@@ -96,10 +43,9 @@ public class AxialMaterialTest extends UnitTest {
       }
    }
 
-   public void testDfdl (
-      AxialMaterial mat, double l, double ldot, double l0, double tol) {
-
-      double ex = 0.5;
+   public void checkDfdl (
+      AxialMaterial mat, double l, double ldot,
+      double l0, double ex, double maxval, double tol) {
 
       double h = 1e-8;
 
@@ -107,10 +53,9 @@ public class AxialMaterialTest extends UnitTest {
       double dFdl = mat.computeDFdl (l, ldot, l0, ex);
       double dFdlChk = (mat.computeF (l+h*l, ldot, l0, ex) - F)/(h*l);
 
-      double mag = Math.abs(dFdl);
       double err = Math.abs(dFdl-dFdlChk);
-      if (mag > 0) {
-         err /= mag;
+      if (maxval > 0) {
+         err /= maxval;
       }
       if (err > tol || err != err) {
          System.out.println (
@@ -118,6 +63,7 @@ public class AxialMaterialTest extends UnitTest {
          System.out.println ("F=" + F);
          System.out.println ("dFdl computed=" + dFdl);
          System.out.println ("dFdl numeric=" + dFdlChk);
+         System.out.println ("l=" + l + " v=" + ldot);
          if (err != err) {
             throw new TestException (
                "Error between numeric and computed dFdl is NaN");
@@ -129,26 +75,165 @@ public class AxialMaterialTest extends UnitTest {
       }
    }
 
+   public void testMaterial (
+      AxialMaterial mat, double l0, double ex, double tol) {
+      testMaterial (mat, mat, 2*l0, 0.2*l0, l0, ex, tol);
+   }
+
+   public void testDfdlOverRange (
+      AxialMaterial mat,
+      double lmin, double lmax, double vmin, double vmax,
+      double l0, double ex, double tol) {
+      
+      int nl = 1; // number of l values to test
+      int nv = 1; // number of v values to test
+      double linc = 0; // l value increment
+      double vinc = 0; // v value increment
+      if (lmin != lmax) {
+         nl = 40;
+         linc = (lmax-lmin)/(nl-1);
+      }
+      if (vmin != vmax) {
+         nv = 20;
+         vinc = (vmax-vmin)/(nv-1);
+      }
+      // first find the magnitude over the range
+      double maxDfdl = 0;
+      for (int i=0; i<nl;  i++) {
+         double l = lmin + i*linc;
+         for (int j=0; j<nv; j++) {
+            double v = vmin + j*vinc;
+            double dfdl = mat.computeDFdl (l, v, l0, ex);
+            if (Math.abs(dfdl) > maxDfdl) {
+               maxDfdl = Math.abs(dfdl);
+            }
+         }
+      }
+      // then do the numeric check
+      for (int i=0; i<nl; i++) {
+         double l = lmin + i*linc;
+         for (int j=0; j<nv; j++) {
+            double v = vmin + j*vinc;
+            checkDfdl (mat, l, v, l0, ex, maxDfdl, tol);
+         }
+      }
+   }
+
+   public void testDfdldotOverRange (
+      AxialMaterial mat, 
+      double lmin, double lmax, double vmin, double vmax,
+      double l0, double ex, double tol) {
+
+      int nl = 1; // number of l values to test
+      int nv = 1; // number of v values to test
+      double linc = 0; // l value increment
+      double vinc = 0; // v value increment
+      if (lmin != lmax) {
+         nl = 40;
+         linc = (lmax-lmin)/(nl-1);
+      }
+      if (vmin != vmax) {
+         nv = 20;
+         vinc = (vmax-vmin)/(nv-1);
+      }
+      // first find the magnitude over the range
+      double maxDfdldot = 0;
+      for (int i=0; i<nl;  i++) {
+         double l = lmin + i*linc;
+         for (int j=0; j<nv; j++) {
+            double v = vmin + j*vinc;
+            double dfdldot = mat.computeDFdldot (l, v, l0, ex);
+            if (Math.abs(dfdldot) > maxDfdldot) {
+               maxDfdldot = Math.abs(dfdldot);
+            }
+         }
+      }
+      // then do the numeric check
+      for (int i=0; i<nl;  i++) {
+         double l = lmin + i*linc;
+         for (int j=0; j<nv; j++) {
+            double v = vmin + j*vinc;
+            checkDfdldot (mat, l, v, l0, ex, maxDfdldot, tol);
+         }
+      }
+   }
+
    /**
-    * Test method executed by runtest().
+    * Tests a material by comparing the computed derivatives with the numeric
+    * derivative of the same. Different materials are specified for testing
+    * dFdl and dFdldot in case different parameter settings are needed to
+    * numerically compute the different derivates.
+    *
+    * @param mat material to test for dFdl
+    * @param matdot material to test for dFdldot
+    * @param tol tolerance by which the computed and numeric derivatives should
+    * match. Typically, this should be around 1e-8.
     */
-   public void test () {
+   public void testMaterial (
+      AxialMaterial mat, AxialMaterial matdot,
+      double l, double ldot, double l0, double ex, double tol) {
 
-      // numeric and analytical tangents should match within this tolerance
-      double tol = 1e-6;
+      testDfdlOverRange (mat, l, l, ldot, ldot, l0, ex, tol);
+      testDfdldotOverRange (matdot, l, l, ldot, ldot, l0, ex, tol);
+   }
 
-      testMaterial (new LinearAxialMaterial(1000.0, 200.0), 1.0, tol);   
-      testMaterial (new SimpleAxialMuscle (1000.0, 200.0, 300), 1.0, tol);
-      testMaterial (new LigamentAxialMaterial (1234.00, 222.0, 333.0), 2.0, tol);
-      testMaterial (new ConstantAxialMuscle (20.0), 2.0, tol);
-      testMaterial (new ConstantAxialMaterial (222.0), 2.0, tol);
-      testMaterial (new LinearAxialMuscle (20, 2.0), 2.0, tol);
-      testMaterial (
-         new PeckAxialMuscle (20.0, 2.0, 3.0, 0.2, 0.5, 0.1), 2.0, 1e-5);
-      testMaterial (
-         new PaiAxialMuscle (20.0, 2.0, 3.0, 0.2, 0.5, 0.1), 2.0, 1e-5);
-      testMaterial (new BlemkerAxialMuscle (1.4, 1.0, 3e5, 0.05, 6.6), 2.0, tol);
-      testMaterial (new MasoudMillardLAM (2.0, 1.8, 0.5), 2.0, tol);
+   /**
+    * Tests a material by comparing the computed derivatives with the numeric
+    * derivative of the same.
+    *
+    * @param mat material to test
+    * @param tol tolerance by which the computed and numeric derivatives should
+    * match. Typically, this should be around 1e-8.
+    */
+   public void testMaterial (
+      AxialMaterial mat, double l, double ldot,
+      double l0, double ex, double tol) {
+
+      testDfdlOverRange (mat, l, l, ldot, ldot, l0, ex, tol);
+      testDfdldotOverRange (mat, l, l, ldot, ldot, l0, ex, tol);
+   }
+
+   public void testMillard2012 (double tol) {
+      double ex = 0.5;
+      Millard2012AxialMuscle mmat = new Millard2012AxialMuscle (
+         2.0, 2.0, 0.6, Math.toRadians(30));
+
+      double maxL = 2.0;
+      double minL = 0.2;
+      double maxV = 2.5;
+      double minV = -2.5;
+
+      mmat.setFibreDamping (1.0);
+      mmat.setIgnoreForceVelocity (true);
+      testDfdlOverRange (mmat, minL, maxL, minV, maxV, 0, ex, 1e-7);
+      testDfdldotOverRange (mmat, minL, maxL, minV, maxV, 0, ex, 2e-7);
+      mmat.setIgnoreForceVelocity (false);
+      testDfdlOverRange (mmat, minL, maxL, minV, maxV, 0, ex, 1e-7);
+      testDfdldotOverRange (mmat, minL, maxL, minV, maxV, 0, ex, 2e-7);
+   }
+
+   public void testBlankevoort1991Ligament (double tol) {
+      double ex = 0.5;
+
+      Blankevoort1991AxialLigament lmat = new Blankevoort1991AxialLigament();
+      lmat.setLinearStiffness (500.0);
+      double sl = 0.08226920;
+      lmat.setSlackLength (sl);
+      Blankevoort1991AxialLigament lmatdot = lmat.clone();
+      lmatdot.setLinearStiffness (0);
+      double lt = sl*(lmat.getTransitionStrain()+1);
+
+      double maxL = lt+(lt-sl)/4;
+      double minL = sl-(lt-sl)/4;
+      double maxV = 0.1*(minL+maxL)/2;
+      double minV = maxV;
+
+      testDfdlOverRange (lmat, minL, maxL, minV, maxV, 0, ex, 10*tol);
+      testDfdldotOverRange (lmatdot, minL, maxL, minV, maxV, 0, ex, 2*tol);
+   }
+
+   public void testHill3ElemMuscleRigidTendon (double tol) {
+      double ex = 0.5;
 
       double T = 0.1; // tendon slack length
       double L = 0.5;  // optimalFibreLen
@@ -161,19 +246,50 @@ public class AxialMaterialTest extends UnitTest {
       mat.setForceScaling (5.0);
       double maxL = T+Math.sqrt(4*L*L-h*h);
       double minL = T+Math.sqrt(4*L*L-h*h)/2.0;
+      double maxV = 0.1*(minL+maxL)/2;
+      double minV = maxV;
       Hill3ElemMuscleRigidTendon matdot = mat.clone();
       matdot.setMaxForce (0);
 
       //testOverRange (mat, minL, maxL, 1.0, 1e-5);
       //testMaterial (mat, 1.0, 1e-5);
-      testOverRange (mat, matdot, minL, maxL, 1.0, tol);
+      testDfdlOverRange (mat, minL, maxL, minV, maxV, 1.0, ex, tol);
+      testDfdldotOverRange (matdot, minL, maxL, minV, maxV, 1.0, ex, tol);
+   }
+
+   /**
+    * Test method executed by runtest().
+    */
+   public void test () {
+         
+      // numeric and analytical tangents should match within this tolerance
+      double tol = 1e-6;
+      double ex = 0.5;
+
+      testMaterial (new LinearAxialMaterial(1000.0, 200.0), 1.0, ex, tol);   
+      testMaterial (new SimpleAxialMuscle (1000.0, 200.0, 300), 1.0, ex, tol);
+      testMaterial (
+         new LigamentAxialMaterial (1234.00, 222.0, 333.0), 2.0, ex, tol);
+      testMaterial (new ConstantAxialMuscle (20.0), 2.0, ex, tol);
+      testMaterial (new ConstantAxialMaterial (222.0), 2.0, ex, tol);
+      testMaterial (new LinearAxialMuscle (20, 2.0), 2.0, ex, tol);
+      testMaterial (
+         new PeckAxialMuscle (20.0, 2.0, 3.0, 0.2, 0.5, 0.1), 2.0, ex, 1e-5);
+      testMaterial (
+         new PaiAxialMuscle (20.0, 2.0, 3.0, 0.2, 0.5, 0.1), 2.0, ex, 1e-5);
+      testMaterial (
+         new BlemkerAxialMuscle (1.4, 1.0, 3e5, 0.05, 6.6), 2.0, ex, tol);
+      testMaterial (new MasoudMillardLAM (2.0, 1.8, 0.5), 2.0, ex, tol);
+
+      testHill3ElemMuscleRigidTendon(5*tol);
+      testBlankevoort1991Ligament(tol);
+      testMillard2012(1e-5);      
    }
 
    public static void main (String[] args) {
       RandomGenerator.setSeed (0x1234);
 
       AxialMaterialTest tester = new AxialMaterialTest();
-
       tester.runtest();
    }
 
--- src/artisynth/core/materials/AxialMuscleMaterialBase.java
+++ src/artisynth/core/materials/AxialMuscleMaterialBase.java
@@ -4,12 +4,14 @@ import java.io.*;
 import java.util.*;
 
 import maspack.interpolation.CubicHermiteSpline1d;
+import maspack.interpolation.CubicHermiteSpline1d.Knot;
 
 import artisynth.core.util.ScalableUnits;
 import artisynth.core.util.ScanToken;
 import artisynth.core.modelbase.*;
 import maspack.properties.*;
 import maspack.util.*;
+import maspack.matrix.*;
 
 /**
  * Base class for point-to-point muscle materials that contains optional
@@ -180,4 +182,342 @@ public abstract class AxialMuscleMaterialBase extends AxialMaterial {
       return super.scanItem (rtok, tokens);
    }
 
+   public static CubicHermiteSpline1d createActiveForceLengthCurve (
+      double minActiveNormFiberLength,
+      double transitionNormFiberLength,
+      double maxActiveNormFiberLength,
+      double shallowAscendingSlope,
+      double minimumValue) {
+
+      if (minActiveNormFiberLength <= 0) {
+         throw new IllegalArgumentException (
+            "minActiveNormFiberLength must be > 0");
+      }
+      if (transitionNormFiberLength >= 1) {
+         throw new IllegalArgumentException (
+            "transitionNormFiberLength must be < 1");
+      }
+      if (minActiveNormFiberLength >= transitionNormFiberLength) {
+         throw new IllegalArgumentException (
+            "minActiveNormFiberLength must be < transitionNormFiberLength");
+      }
+      if (maxActiveNormFiberLength <= 1) {
+         throw new IllegalArgumentException (
+            "maxActiveNormFiberLength must be > 1");
+      }
+      if (shallowAscendingSlope < 0) {
+         throw new IllegalArgumentException (
+            "shallowAscendingSlope must be >= 0");
+      }
+      if (shallowAscendingSlope >= 1/(1-transitionNormFiberLength)) {
+         throw new IllegalArgumentException (
+            "shallowAscendingSlope must be < 1/(1-transitionNormFiberLength)");
+      }
+      if (minimumValue < 0) {
+         throw new IllegalArgumentException (
+            "minimumValue must be >= 0");
+      }
+      
+      CubicHermiteSpline1d curve = new CubicHermiteSpline1d();
+
+      // start point
+      curve.addKnot (minActiveNormFiberLength, minimumValue, 0.0);
+
+      // trans point
+      double xt = transitionNormFiberLength;
+      curve.addKnot (
+         xt, 1.0 - shallowAscendingSlope*(1.0-xt), shallowAscendingSlope);
+
+      // max point
+      curve.addKnot (1.0, 1.0, 0.0);
+
+      // end point
+      curve.addKnot (maxActiveNormFiberLength, minimumValue, 0);
+      return curve;
+   }
+
+   /**
+    * Check for an inflexion point within the curve interval specified by the
+    * {@code idx}-th knot, and if one is found, try to remove it by adding an
+    * additional knot the interval.
+    */
+   static void addAntiInflexionKnot (CubicHermiteSpline1d curve, int idx0) {
+
+      Knot knot0 = curve.getKnot(idx0);
+      Knot knot1 = curve.getKnot(idx0+1);
+
+      // unpack the x, y and dy parameters at each knot. 
+      double x0 = knot0.getX();
+      double x1 = knot1.getX();
+      double h = x1-x0; // length of the interval
+      double y0 = knot0.getY();
+      double dy0 = knot0.getDy();
+      double y1 = knot1.getY();
+      double dy1 = knot1.getDy();
+
+      // upack the a3 and a2 cubic coefficients for knot0. These are related
+      // the initial acceleration y''(0) and (constant) jerk value y'''(x) by
+      //
+      // y''(0) = 2 a2
+      // y'''(x) = 6 a3
+      //
+      // Note: each knot's cubic and associated coefficients are defined with
+      // respect to the shifted interval [0 x1-x0].
+      double a2 = knot0.getA2();
+      double a3 = knot0.getA3();
+
+      // see if there is an inflexion point in the interval.  This will be true
+      // if y''(x) has a zero. If it does not, there is no need to do anything.
+      if (a3 != 0) {
+         double chk = -a2/(3*a3);
+         if (chk <= 0 && chk >= h) {
+            // no inflexion
+            return;
+         }
+      }
+      if (dy1-dy0 == 0) {
+         // can't remove inflexion if there is no end point change y'(x)
+         return;
+      }
+      // Try to remove the inflexion by adding an extra knot at x = tau in the
+      // interval [0, h], and replace the straight line defining y''(x) with a
+      // piecewise linear version with curvature values c0, ctau and c1
+      // defined at x = {0, tau, h}.
+      if (Math.abs(dy0) > Math.abs(dy1)) {
+         // forward direction: curvature at the beginning and set c1 = 0.
+         // See if solutuin is feasible by solving for tau with ctau = 0.
+         double tau = 3*(dy1*h-(y1-y0))/(dy1-dy0);
+         if (tau <= 0 || tau >= h) {
+            // can't do it - no solution
+            return;
+         }
+         // If feasible, reduce tau by 2/3 to get a soother curve:
+         tau = 2*tau/3;
+         // Now backsolve for c0 and ctau:
+         Vector2d cvec = new Vector2d();
+         Vector2d b = new Vector2d (y1-y0+(dy1-dy0)*tau - h*dy1, dy1-dy0);
+         Matrix2d M = new Matrix2d (
+            tau*tau/3, (2*tau-h)*h/6, tau/2, h/2);
+         double d = Matrix2d.solve (M, b, cvec);
+         double c0 = cvec.x;
+         double ctau = cvec.y;
+         double ytau = y0 + dy0*tau + (c0/3 + ctau/6)*tau*tau;
+         double dytau = dy0 + (c0 + ctau)*tau/2;
+         curve.addKnot (x0+tau, ytau, dytau);
+      }
+      else {
+         // other direction: apply curvature at interval end and set c0 = 0.
+         double tau = 3*((y1-y0)-dy0*h)/(dy1-dy0);
+         if (tau <= 0 || tau >= h) {
+            // can't do it - no solution
+            return;
+         }
+         // reduce tau by 2/3 to get a soother curve:
+         tau = 2*tau/3;
+         //  backsolve for c1 and ctau:
+         Vector2d cvec = new Vector2d();
+         Vector2d b = new Vector2d (y1-y0-(dy1-dy0)*tau - h*dy0, dy1-dy0);
+         Matrix2d M = new Matrix2d (
+            -tau*tau/3, -(2*tau-h)*h/6, tau/2, h/2);
+         double d = Matrix2d.solve (M, b, cvec);
+         double ctau = cvec.y;
+         double ytau = y0 + dy0*(h-tau) + ctau*(h-tau)*(h-tau)/6;
+         double dytau = dy0 + ctau*(h-tau)/2;
+         curve.addKnot (x0+h-tau, ytau, dytau);
+      }
+   }
+
+   public static CubicHermiteSpline1d createForceVelocityCurve (
+      double concentricSlopeAtVmax,
+      double concentricSlopeNearVmax,
+      double isometricSlope,
+      double eccentricSlopeAtVmax, 
+      double eccentricSlopeNearVmax,
+      double maxEccentricVelocityForceMultiplier,
+      double concentricCurviness,
+      double eccentricCurviness) {
+
+      // check conditions
+      if (concentricSlopeAtVmax < 0 || concentricSlopeAtVmax >= 1) {
+         throw new IllegalArgumentException (
+            "concentricSlopeAtVmax must be in the range [0,1)");
+      }
+      if (concentricSlopeNearVmax <= concentricSlopeAtVmax ||
+          concentricSlopeNearVmax >= isometricSlope) {
+         throw new IllegalArgumentException (
+            "concentricSlopeNearVmax must be > concentricSlopeAtVmax "+
+            "and < isometricSlope");
+      }
+      if (isometricSlope <= 1) {
+         throw new IllegalArgumentException (
+            "isometricSlope must be > 1");
+      }
+      if (eccentricSlopeNearVmax >= isometricSlope ||
+          eccentricSlopeNearVmax <= eccentricSlopeAtVmax) {
+         throw new IllegalArgumentException (
+            "eccentricSlopeNearVmax must be < isometricSlope "+
+            "and > eccentricSlopeAtVmax");
+      }
+      if (isometricSlope <= maxEccentricVelocityForceMultiplier-1) {
+         throw new IllegalArgumentException (
+            "isometricSlope must exceed (maxEccentricVelocityForceMultiplier-1)");
+      }
+      if (eccentricSlopeAtVmax < 0) {
+         throw new IllegalArgumentException (
+            "eccentricSlopeAtVmax must be >= 0");
+      }
+      if (eccentricSlopeAtVmax >= maxEccentricVelocityForceMultiplier-1) {
+         throw new IllegalArgumentException (
+            "eccentricSlopeAtVmax must be less than " +
+            "(maxEccentricVelocityForceMultiplier-1)");
+      }
+      if (maxEccentricVelocityForceMultiplier <= 1) {
+         throw new IllegalArgumentException (
+            "maxEccentricVelocityForceMultiplier must be > 1");
+      }
+      if (concentricCurviness < 0 || concentricCurviness > 1) {
+         throw new IllegalArgumentException (
+            "concentricCurviness must be in the range [0,1]");
+      }
+      if (eccentricCurviness < 0 || eccentricCurviness > 1) {
+         throw new IllegalArgumentException (
+            "eccentricCurviness must be in the range [0,1]");
+      }
+      // need to make end points have small non-zero slopes to ensure curve is
+      // invertible.
+
+      CubicHermiteSpline1d curve = new CubicHermiteSpline1d();
+
+      // start point at C
+      double xC = -1.0;
+      double yC = 0.0;
+      double dydxC = concentricSlopeAtVmax;
+      curve.addKnot (xC, yC, dydxC);
+
+      // point near C
+      double xNearC = -0.9;
+      double dydxNearC = concentricSlopeNearVmax;
+      double yNearC = yC + 0.5*dydxNearC*(xNearC-xC) + 0.5*dydxC*(xNearC-xC);
+      curve.addKnot (xNearC, yNearC, dydxNearC);
+
+      // ISO mid point
+      curve.addKnot (0.0, 1.0, isometricSlope);
+
+      // end point E
+      double xE = 1.0;
+      double yE = maxEccentricVelocityForceMultiplier;
+      double dydxE = eccentricSlopeAtVmax;
+      curve.addKnot (xE, yE, dydxE);
+
+      // point near E
+      double xNearE = 0.9;
+      double dydxNearE = eccentricSlopeNearVmax;
+      double yNearE =
+         yE + 0.5*dydxNearE*(xNearE-xE) + 0.5*dydxE*(xNearE-xE);
+      curve.addKnot (xNearE, yNearE, dydxNearE);
+
+      // add extra knot to remove inflection between near C and ISO
+      addAntiInflexionKnot (curve, 1);
+      // add extra knot to remove inflection between near ISO and near E
+      addAntiInflexionKnot (curve, 3);
+
+      return curve;
+   }
+
+   public static CubicHermiteSpline1d createTendonForceLengthCurve (
+      double strainAtOneNormForce,
+      double stiffnessAtOneNormForce,
+      double normForceAtToeEnd,
+      double curviness) {
+
+      if (strainAtOneNormForce <= 0) {
+         throw new IllegalArgumentException (
+            "strainAtOneNormForce must be > 0");
+      }
+      if (stiffnessAtOneNormForce <= 1/strainAtOneNormForce) {
+         throw new IllegalArgumentException (
+            "stiffnessAtOneNormForce must be > 1/strainAtOneNormForce");
+      }
+      if (normForceAtToeEnd <= 0 || normForceAtToeEnd >= 1) {
+         throw new IllegalArgumentException (
+            "normForceAtToeEnd must be in the range (0,1)");
+      }
+      if (curviness < 0 || curviness > 1) {
+         throw new IllegalArgumentException (
+            "curviness must be in the range [0,1]");
+      }
+
+      double eIso = strainAtOneNormForce;
+      double kIso = stiffnessAtOneNormForce;
+      double fToe = normForceAtToeEnd;
+
+      CubicHermiteSpline1d curve = new CubicHermiteSpline1d();
+
+      // start point
+      curve.addKnot (1.0, 0.0, 0.0);
+
+      // ISO point
+      double xIso = 1.0 + strainAtOneNormForce;
+      curve.addKnot (xIso, 1.0, stiffnessAtOneNormForce);
+      
+      // point between start and ISO where the curved section becomes linear
+      curve.addKnot ((fToe-1)/kIso + xIso, fToe, stiffnessAtOneNormForce);
+      
+      return curve;
+   }
+      
+   public static CubicHermiteSpline1d createPassiveForceLengthCurve (
+      double strainAtZeroForce,
+      double strainAtOneNormForce,
+      double stiffnessAtLowForce,
+      double stiffnessAtOneNormForce, 
+      double curviness) {
+
+      if (strainAtZeroForce >= strainAtOneNormForce) {
+         throw new IllegalArgumentException (
+            "strainAtZeroForce must be < strainAtOneNormForce");
+      }
+      if (stiffnessAtOneNormForce <= 1/(strainAtOneNormForce-strainAtZeroForce)) {
+         throw new IllegalArgumentException (
+            "stiffnessAtOneNormForce must be > "+
+            "1/(strainAtOneNormForce-strainAtZeroForce)");
+      }
+      if (stiffnessAtLowForce <= 0) {
+         throw new IllegalArgumentException (
+            "stiffnessAtLowForce must be > 0");
+      }
+      if (stiffnessAtLowForce >= stiffnessAtOneNormForce) {
+         throw new IllegalArgumentException (
+            "stiffnessAtLowForce must be < stiffnessAtOneNormForce");
+      }
+      if (curviness < 0 || curviness > 1) {
+         throw new IllegalArgumentException (
+            "curviness must be in the range [0,1]");
+      }
+      
+      double eZero = strainAtZeroForce;
+      double eIso = strainAtOneNormForce;
+      double kLow = stiffnessAtLowForce;
+      double kIso = stiffnessAtOneNormForce;
+
+      CubicHermiteSpline1d curve = new CubicHermiteSpline1d();
+
+      // start point
+      double xStart = 1.0 + eZero;
+      curve.addKnot (xStart, 0.0, 0.0);
+
+      // ISO point
+      double xIso = 1.0 + eIso;
+      curve.addKnot (xIso, 1.0, stiffnessAtOneNormForce);
+
+      // intermediate knot between start and ISO
+      double deltaX = Math.min(0.1*(1.0/kIso), 0.1*(xIso-xStart));
+      double xLow = xStart + deltaX;
+      double xfoot = xStart + 0.5*(xLow-xStart);
+      curve.addKnot (xStart + deltaX, kLow*(xLow-xfoot), kLow);
+      
+      return curve;
+   }
+
 }
+

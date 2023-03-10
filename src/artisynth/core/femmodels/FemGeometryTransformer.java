7c9b23815d86fb74dac516aadd1ea3738c583cd0
John Lloyd <lloyd@ece.ubc.ca>
2023-03-10 11:33:30 -0800

    Added a distance limit to FemGeometryTransformer outside of which the deformation field is not applied.

--- src/artisynth/core/femmodels/FemGeometryTransformer.java
+++ src/artisynth/core/femmodels/FemGeometryTransformer.java
@@ -13,6 +13,33 @@ public class FemGeometryTransformer extends DeformationTransformer {
    FemModel3d myFem;
    FemModel3d myRestFem;
 
+   double myMaxDistance = -1;
+
+   /**
+    * Set a maximum distance {@code maxd} (in the rest configuration) beyond
+    * which the deformation field is not applied.  Specifically, no deformation
+    * is applied at points whose rest distance from the FEM position exceeds
+    * {@code maxd}. A negative value implies to maximum distance is in
+    * effect. The default value is -1.
+    *
+    * @param maxd maximum rest distance
+    * @see getMaxDistance
+    */
+   public void setMaxDistance (double maxd) {
+      myMaxDistance = maxd;
+   }
+
+   /**
+    * Queries the maximum distance beyond which the deformation field is not
+    * applied.
+    *
+    * @return maxd maximum rest distance
+    * @see setMaxDistance
+    */
+   public double getMaxDistance (double maxd) {
+      return myMaxDistance;
+   }
+
    /**
     * Creates a FemGeometry transformer for the specified FEM model
     * 
@@ -29,31 +56,43 @@ public class FemGeometryTransformer extends DeformationTransformer {
    public void getDeformation (Vector3d p, Matrix3d F, Vector3d r) {
       
       Point3d rpos = new Point3d(r);
-      FemElement3dBase restElem = myRestFem.findNearestElement (null, rpos);
-      Vector3d coords = new Vector3d();
-      if (!restElem.getNaturalCoordinates (coords, rpos)) {
-         //System.out.println ("warning...");
+      Point3d loc = new Point3d();
+      FemElement3dBase restElem = myRestFem.findNearestElement (loc, rpos);
+      if (loc.distance (rpos) <= myMaxDistance) {
+         Vector3d coords = new Vector3d();
+         if (!restElem.getNaturalCoordinates (coords, rpos)) {
+            //System.out.println ("warning...");
+         }
+         IntegrationPoint3d ipnt =
+            IntegrationPoint3d.create (restElem, coords.x, coords.y, coords.z, 1);
+         Matrix3d invJ0 = new Matrix3d();
+         double detJ0 = 
+            ipnt.computeInverseRestJacobian (invJ0, restElem.getNodes());
+         if (detJ0 <= 0) {
+            //System.out.println ("warning...");
+         }
+         
+         int elemIdx = myRestFem.getElements().indexOf(restElem);
+         FemElement3d elem = myFem.getElements().get(elemIdx);
+         
+         if (p != null) {
+            Point3d pos = new Point3d();
+            ipnt.computePosition (pos, elem.getNodes());
+            p.set (pos);         
+         }
+         if (F != null) {
+            ipnt.computeGradient (F, elem.getNodes(), invJ0);
+            //F.set (ipnt.getF());
+         }
       }
-      IntegrationPoint3d ipnt =
-         IntegrationPoint3d.create (restElem, coords.x, coords.y, coords.z, 1);
-      Matrix3d invJ0 = new Matrix3d();
-      double detJ0 = 
-         ipnt.computeInverseRestJacobian (invJ0, restElem.getNodes());
-      if (detJ0 <= 0) {
-         //System.out.println ("warning...");
-      }
-
-      int elemIdx = myRestFem.getElements().indexOf(restElem);
-      FemElement3d elem = myFem.getElements().get(elemIdx);
-
-      if (p != null) {
-         Point3d pos = new Point3d();
-         ipnt.computePosition (pos, elem.getNodes());
-         p.set (pos);         
-      }
-      if (F != null) {
-         ipnt.computeGradient (F, elem.getNodes(), invJ0);
-         //F.set (ipnt.getF());
+      else {
+         // do nothing - field is ignored
+         if (p != null) {
+            p.set (r);
+         }
+         if (F != null) {
+            F.setIdentity();
+         }
       }
    }
 

38ea4c91f0184d65388b87e3765dd7df0611acc1
John Lloyd <lloyd@ece.ubc.ca>
2024-07-28 17:34:06 -0700

    Added comments to SimpleBall.

--- src/artisynth/models/testspace/SimpleBall.java
+++ src/artisynth/models/testspace/SimpleBall.java
@@ -4,12 +4,16 @@ import artisynth.core.workspace.RootModel;
 import artisynth.core.mechmodels.RigidBody;
 import artisynth.core.mechmodels.MechModel;
 
+// All ArtiSynth models extend RootModel
 public class SimpleBall extends RootModel {
 
+   // build method assembles the model
    public void build (String[] args) {
+      // create a MechModel to contain other mechanical components
       MechModel mech = new MechModel ("mech");
       addModel (mech);
 
+      // create a spherical ball and add it to the MechModel
       RigidBody ball = RigidBody.createSphere (
          "ball", /*radius*/.1, /*density*/1000, /*nslices*/32);
       mech.addRigidBody (ball);
